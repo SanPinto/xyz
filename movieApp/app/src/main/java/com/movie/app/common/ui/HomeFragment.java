3fda36f9b8ad79917912d4b8249c4e0284d7109f
# <#>
2017-10-31 07:16:02 +0530

    Added Sliding Tab theme

--- movieApp/app/src/main/java/com/movie/app/common/ui/HomeFragment.java
+++ movieApp/app/src/main/java/com/movie/app/common/ui/HomeFragment.java
@@ -21,6 +21,7 @@ import java.util.List;
 public class HomeFragment extends Fragment {
     private static final String NAVIGATION_LIST = "navigation_list";
     private static final String CLICKED_NAV_POS = "clicked_nav_pos";
+    private View mSlidingTab;
 
     public static Fragment getInstance(List<NavigationItem> navigationItemList, int pos) {
         HomeFragment fragment = new HomeFragment();
@@ -36,5 +37,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+    private void initViews(View view) {
+        mSlidingTab = view.findViewById(R.id.sliding_tab);
     }
 }
